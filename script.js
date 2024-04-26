980e2248416329c72e931f99b2d47b986851f6b7
alemran22 <alemrangazi@gmail.com>
2024-04-26 18:59:11 +0600

    slick slider

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 
 $(document).ready(function(){
   $('.container').slick({
+    // dots
+    dots: true,
+    // multiple item
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // center mode
+    centerMode: false,
+    // centerPadding:"60px",
+   
+    // lazy load
+    // lazyLoad: "ondemand",
+    // autoplay
+      
+    // slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
+   
+   // fade
+    // infinite: true,
+    speed: 500,
+    fade: false,
+    cssEase: 'linear',
+    // arrows
+    arrows :true,
+    prevArrow: ".backbutton",
+    nextArrow: ".nextbutton",
+    // drag by mouse
+    draggable: true,
+    // focusOnSelect
+    focusOnSelect: true,
+    // initialSlide
+    initialSlide: 0,
+    
     
   });
+ 
 });
 	
