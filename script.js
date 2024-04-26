5d7c97b9b1d0a881739600c8b1e1973eda3d7149
alemran22 <alemrangazi@gmail.com>
2024-04-26 21:33:11 +0600

    update

--- script.js
+++ script.js
@@ -4,36 +4,36 @@ $(document).ready(function(){
     // dots
     dots: true,
     // multiple item
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // center mode
-    centerMode: false,
-    // centerPadding:"60px",
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   // center mode
+  //   centerMode: false,
+  //   // centerPadding:"60px",
    
-    // lazy load
-    // lazyLoad: "ondemand",
-    // autoplay
+  //   // lazy load
+  //   // lazyLoad: "ondemand",
+  //   // autoplay
       
-    // slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 3000,
+  //   // slidesToScroll: 1,
+  //   autoplay: false,
+  //   autoplaySpeed: 3000,
    
-   // fade
-    // infinite: true,
-    speed: 500,
-    fade: false,
-    cssEase: 'linear',
-    // arrows
-    arrows :true,
-    prevArrow: ".backbutton",
-    nextArrow: ".nextbutton",
-    // drag by mouse
-    draggable: true,
-    // focusOnSelect
-    focusOnSelect: true,
-    // initialSlide
-    initialSlide: 0,
+  //  // fade
+  //   // infinite: true,
+  //   speed: 500,
+  //   fade: false,
+  //   cssEase: 'linear',
+  //   // arrows
+  //   arrows :true,
+  //   prevArrow: ".backbutton",
+  //   nextArrow: ".nextbutton",
+  //   // drag by mouse
+  //   draggable: true,
+  //   // focusOnSelect
+  //   focusOnSelect: true,
+  //   // initialSlide
+  //   initialSlide: 0,
     
     
   });
